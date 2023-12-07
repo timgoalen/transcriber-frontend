1c3cc60d49cea64f6c09c73c350dba42d0f818fd
Tim Goalen <timgoalen@hotmail.com>
2023-12-07 14:31:15 +0000

    Add static layout oflanding page

--- transcriber-frontend/src/App.js
+++ transcriber-frontend/src/App.js
@@ -1,33 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faArrowUpFromBracket,
+  faListUl,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+
 function Header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
+      <div className="list-view-btn-container">
+        <a href="list-page.html">
+          <FontAwesomeIcon icon={faListUl} />
+        </a>
+      </div>
     </header>
   );
 }
 
 function TextArea() {
-  return <textarea></textarea>;
+  return (
+    <div id="main-container">
+        <section id="text-container">
+          <textarea id="text-area"></textarea>
+          {/* <div id="canvas-container">
+            <canvas id="audio-visualizer"></canvas>
+          </div> */}
+        </section>
+    </div>
+  );
 }
 
 function MicrophoneIcon() {
-  return <div id="microphone-container">Microphone</div>;
+  return (
+    <div id="microphone-container">
+      <FontAwesomeIcon icon={faMicrophone} />
+    </div>
+  );
 }
 
 function Toolbar() {
   return (
-    <div>
-      <div class="footer-left">
+    <div className="toolbar">
+      <div className="footer-left">
         <div id="save-btn">
-          (save icon)
-          <div class="btn-text">Save</div>
+          <FontAwesomeIcon icon={faArrowUpFromBracket} />
+          <div className="btn-text">Save</div>
         </div>
       </div>
 
-      <div class="footer-right">
+      <div className="footer-right">
         <div id="clear-btn">
-          (clear icon)
-          <div class="btn-text">Clear</div>
+          <FontAwesomeIcon icon={faTrashCan} />
+          <div className="btn-text">Clear</div>
         </div>
       </div>
     </div>
