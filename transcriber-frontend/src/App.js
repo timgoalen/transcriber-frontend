b230136397507a8fa30801dfb3747ae4113facd4
Tim Goalen <timgoalen@hotmail.com>
2023-12-08 14:49:37 +0000

    Add clearBtn function

--- transcriber-frontend/src/App.js
+++ transcriber-frontend/src/App.js
@@ -7,6 +7,20 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
+const notes = [];
+
+function NotesList(props) {
+  const notesArray = props.notes;
+
+  return (
+    <div>
+      {notesArray.map((note, index) => (
+        <div key={index}>{note}</div>
+      ))}
+    </div>
+  );
+}
+
 function Button({ name, icon, onClick }) {
   return (
     <button className="btn-container" onClick={onClick}>
@@ -27,19 +41,35 @@ function Header({ title, onListClick }) {
   );
 }
 
-function TextArea() {
+function TextArea(props) {
   const [textInput, setTextInput] = useState("");
 
-  const handleTextareaChange = (event) => {
+  function handleTextareaChange(event) {
     setTextInput(event.target.value);
-    console.log(textInput);
-  };
+    // console.log(textInput);
+  }
+
+  function handleSaveBtnClick() {
+    // props.createNote(textInput);
+    notes.push(textInput);
+    console.log({notes});
+    console.log({textInput});
+  }
+
+  function handleClearBtnClick() {
+    setTextInput("");
+    console.log({textInput});
+  }
 
   return (
     <>
       <div id="main-container">
         <section id="text-container">
-          <textarea id="text-area" value={textInput} onChange={handleTextareaChange}></textarea>
+          <textarea
+            id="text-area"
+            value={textInput}
+            onChange={handleTextareaChange}
+          ></textarea>
           {/* <div id="canvas-container">
             <canvas id="audio-visualizer"></canvas>
           </div> */}
@@ -48,11 +78,15 @@ function TextArea() {
 
       <section className="toolbar">
         <div className="footer-left">
-          <Button name="Save" icon={faArrowUpFromBracket} />
+          <Button
+            name="Save"
+            icon={faArrowUpFromBracket}
+            onClick={handleSaveBtnClick}
+          />
         </div>
 
         <div className="footer-right">
-          <Button name="Clear" icon={faTrashCan} />
+          <Button name="Clear" icon={faTrashCan} onClick={handleClearBtnClick} />
         </div>
       </section>
     </>
@@ -81,19 +115,22 @@ function MicrophoneIcon({ onMicrophoneClick }) {
 //   );
 // }
 
-// APP
-
+// -- APP --
 let showList = false;
-// const [showList, setShowList] = useState(0);
 
 export default function Transcriber() {
+  function createNote(note) {
+    console.log(note);
+  }
+
   if (!showList) {
     return (
       <>
         <Header title="transcriber" onListClick={() => alert("1")} />
-        <TextArea />
+        <TextArea createNote={createNote} />
         <MicrophoneIcon onMicrophoneClick={() => alert("Mic")} />
         {/* <Toolbar onSave={() => alert("Save")} onClear={() => alert("Clear")} /> */}
+        <NotesList notes={notes} />
       </>
     );
   } else {
