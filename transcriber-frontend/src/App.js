b15fd9e25f4ee841ab0ac1983f1cccbff85a7693
Tim Goalen <timgoalen@hotmail.com>
2023-12-08 13:14:39 +0000

    Add textInput state on textarea

--- transcriber-frontend/src/App.js
+++ transcriber-frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMicrophone,
@@ -5,40 +6,6 @@ import {
   faListUl,
 } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-
-function Header({ title }) {
-  return (
-    <header>
-      <h1>{title}</h1>
-      <div className="list-view-btn-container">
-        <a href="list-page.html">
-          <FontAwesomeIcon icon={faListUl} />
-        </a>
-      </div>
-    </header>
-  );
-}
-
-function TextArea() {
-  return (
-    <div id="main-container">
-      <section id="text-container">
-        <textarea id="text-area"></textarea>
-        {/* <div id="canvas-container">
-            <canvas id="audio-visualizer"></canvas>
-          </div> */}
-      </section>
-    </div>
-  );
-}
-
-function MicrophoneIcon() {
-  return (
-    <div id="microphone-container">
-      <FontAwesomeIcon icon={faMicrophone} />
-    </div>
-  );
-}
 
 function Button({ name, icon, onClick }) {
   return (
@@ -49,27 +16,87 @@ function Button({ name, icon, onClick }) {
   );
 }
 
-function Toolbar({ onSave, onClear }) {
+function Header({ title, onListClick }) {
   return (
-    <section className="toolbar">
-      <div className="footer-left">
-        <Button name="Save" icon={faArrowUpFromBracket} onClick={onSave}/>
+    <header>
+      <h1>{title}</h1>
+      <div className="list-view-btn-container">
+        <Button icon={faListUl} onClick={onListClick} />
       </div>
-
-      <div className="footer-right">
-        <Button name="Clear" icon={faTrashCan} onClick={onClear}/>
-      </div>
-    </section>
+    </header>
   );
 }
 
-export default function Transcriber() {
+function TextArea() {
+  const [textInput, setTextInput] = useState("");
+
+  const handleTextareaChange = (event) => {
+    setTextInput(event.target.value);
+    console.log(textInput);
+  };
+
   return (
     <>
-      <Header title="transcriber" />
-      <TextArea />
-      <MicrophoneIcon />
-      <Toolbar onSave={() => alert("Saved")} onClear={() => alert("Clear")}/>
+      <div id="main-container">
+        <section id="text-container">
+          <textarea id="text-area" value={textInput} onChange={handleTextareaChange}></textarea>
+          {/* <div id="canvas-container">
+            <canvas id="audio-visualizer"></canvas>
+          </div> */}
+        </section>
+      </div>
+
+      <section className="toolbar">
+        <div className="footer-left">
+          <Button name="Save" icon={faArrowUpFromBracket} />
+        </div>
+
+        <div className="footer-right">
+          <Button name="Clear" icon={faTrashCan} />
+        </div>
+      </section>
     </>
   );
+}
+
+function MicrophoneIcon({ onMicrophoneClick }) {
+  return (
+    <button id="microphone-container" onClick={onMicrophoneClick}>
+      <FontAwesomeIcon icon={faMicrophone} />
+    </button>
+  );
+}
+
+// function Toolbar({ onSave, onClear }) {
+//   return (
+//     <section className="toolbar">
+//       <div className="footer-left">
+//         <Button name="Save" icon={faArrowUpFromBracket} onClick={onSave} />
+//       </div>
+
+//       <div className="footer-right">
+//         <Button name="Clear" icon={faTrashCan} onClick={onClear} />
+//       </div>
+//     </section>
+//   );
+// }
+
+// APP
+
+let showList = false;
+// const [showList, setShowList] = useState(0);
+
+export default function Transcriber() {
+  if (!showList) {
+    return (
+      <>
+        <Header title="transcriber" onListClick={() => alert("1")} />
+        <TextArea />
+        <MicrophoneIcon onMicrophoneClick={() => alert("Mic")} />
+        {/* <Toolbar onSave={() => alert("Save")} onClear={() => alert("Clear")} /> */}
+      </>
+    );
+  } else {
+    return <Header title="list" />;
+  }
 }
