6e18c9e2379488b30c09ad3e095b4a54a5fe2147
Tim Goalen <timgoalen@hotmail.com>
2024-02-06 09:35:58 +0000

    feat: add framer-motion to UserMessages

--- src/components/UserMessages.js
+++ src/components/UserMessages.js
@@ -1,7 +1,16 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function UserMessages({ messages }) {
   return (
-    <div className="user-message-container">
-      <p className="user-message-content">{messages}</p>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        transition={{ duration: 0.1 }}
+        initial={{ x: -10, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        className="user-message-container"
+      >
+        <p className="user-message-content">{messages}</p>
+      </motion.div>
+    </AnimatePresence>
   );
 }
