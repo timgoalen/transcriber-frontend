0b4269c33e0b687b048b85986f00a0f9d27c1132
Tim Goalen <timgoalen@hotmail.com>
2024-02-06 10:16:42 +0000

    style: update user message styles

--- src/components/UserMessages.js
+++ src/components/UserMessages.js
@@ -1,16 +1,14 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function UserMessages({ messages }) {
   return (
-    <AnimatePresence>
-      <motion.div
-        transition={{ duration: 0.1 }}
-        initial={{ x: -10, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        className="user-message-container"
-      >
-        <p className="user-message-content">{messages}</p>
-      </motion.div>
-    </AnimatePresence>
+    <motion.div
+      transition={{ duration: 0.1 }}
+      initial={{ x: -10, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      className="user-message-container"
+    >
+      <p className="user-message-content">{messages}</p>
+    </motion.div>
   );
 }
