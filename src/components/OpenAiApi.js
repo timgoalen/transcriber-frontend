86c43d254ebe5c6bde90a9ee50b9eed5a5f5fb38
Tim Goalen <timgoalen@hotmail.com>
2024-02-07 16:21:07 +0000

    feat(OpenAiApi): add validation and undo function

--- src/components/OpenAiApi.js
+++ src/components/OpenAiApi.js
@@ -1,19 +1,48 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 
 import axios from "axios";
-import { faWandMagicSparkles, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import {
+  faWandMagicSparkles,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 
+import { UserMessagesContext } from "../context/UserMessagesContext";
 import Button from "./Button.js";
 
 export default function OpenAiApi({ textAreaInput, setTextAreaInput }) {
-  const [response, setResponse] = useState("");
+  const [unformattedNote, setUnformattedNote] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [showUndo, setShowUndo] = useState(false);
+  const [isUndoListenerOn, setIsUndoLIstenerOn] = useState(false);
+  const { addToMessages } = useContext(UserMessagesContext);
+
+  // After formatting, hide undo button when user enters more text
+  useEffect(() => {
+    function removeUndoBtnOnKeyPress() {
+      setShowUndo(false);
+      setIsUndoLIstenerOn(false);
+    };
+
+    window.addEventListener('keydown', removeUndoBtnOnKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', removeUndoBtnOnKeyPress);
+    };
+  }, [isUndoListenerOn]); 
+  
+  
+  function undoAiResponse() {
+    setTextAreaInput(unformattedNote);
+    setShowUndo(false);
+    addToMessages("undone");
+  }
 
   async function sendToOpenAiApi() {
     // Handle when text area is empty
-    // if (textAreaInput === "") {
-    //   return;
-    // }
+    if (textAreaInput === "") {
+      addToMessages("can't format an empty note");
+      return;
+    }
 
     try {
       setIsLoading(true);
@@ -36,22 +65,33 @@ export default function OpenAiApi({ textAreaInput, setTextAreaInput }) {
       );
       const reply = response.data.choices[0].text.trim();
       setIsLoading(false);
+      setUnformattedNote(textAreaInput);
       setTextAreaInput(reply);
-      // setResponse(response.data.choices[0].text);
+      setShowUndo(true);
+      addToMessages("note formatted");
     } catch (error) {
-      console.error("OpenAI API Error:", error);
       setIsLoading(false);
+      console.error("OpenAI API Error:", error);
+      alert("Error retrieving AI response");
     }
   }
 
-  return (
-    <div id="ai-btn" onClick={() => sendToOpenAiApi()}>
-      {isLoading ? (
-        <Button name={""} icon={faSpinner} ariaLabel="Loading AI Response"/>
-      ) : (
-        <Button name={""} icon={faWandMagicSparkles} ariaLabel="AI Formatting"/>
-
-      )}
+  return showUndo ? (
+    // Undo button
+    <div id="ai-btn">
+      <div className="undo-ai-response" onClick={undoAiResponse}>
+        Undo
+      </div>
+    </div>
+  ) : isLoading ? (
+    // Loading spinner
+    <div id="ai-btn">
+      <Button name={""} icon={faSpinner} ariaLabel="Loading AI Response" />
+    </div>
+  ) : (
+    // AI button
+    <div id="ai-btn" onClick={sendToOpenAiApi}>
+      <Button name={""} icon={faWandMagicSparkles} ariaLabel="AI Formatting" />
     </div>
   );
 }
