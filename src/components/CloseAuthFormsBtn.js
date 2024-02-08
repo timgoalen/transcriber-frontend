c7c20e35ef8aacd4b2ffc99f4e1397753ebe9963
Tim Goalen <timgoalen@hotmail.com>
2024-02-08 13:17:34 +0000

    fix(accessibility): add aria-label to CloseAuthFormsBtn following Lighthouse report

--- src/components/CloseAuthFormsBtn.js
+++ src/components/CloseAuthFormsBtn.js
@@ -3,7 +3,12 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function CloseAuthFormsBtn({ onClick }) {
   return (
-    <button id="cancel-btn" className="close-auth-form-btn" onClick={onClick}>
+    <button
+      id="cancel-btn"
+      className="close-auth-form-btn"
+      onClick={onClick}
+      aria-label="Cancel button"
+    >
       <FontAwesomeIcon icon={faXmark} />
     </button>
   );
